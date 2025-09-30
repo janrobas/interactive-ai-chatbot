d377a2cbd3382368f8fccaff9e0880e71de3b28e
Jan Robas <janrobas@Mac-mini-Jan.local>
2025-09-30 22:15:01 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    QDRANT_URL = "http://localhost:6333"
+    COLLECTION_NAME = "test_collection_1"
+    EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+    LLAMA_MODEL = "llama3.2:1b"
